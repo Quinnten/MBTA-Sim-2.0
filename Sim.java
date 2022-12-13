ce9a671a43532ea3f6e1bb437ad557f3f73192b8
Quinnten P Silveira-Charbonnier <107774771+Quinnten@users.noreply.github.com>
2022-12-12 22:52:02 -0500

    Update Sim.java

--- Sim.java
+++ Sim.java
@@ -1,9 +1,38 @@
 import java.io.*;
+import java.util.*;
 
 public class Sim {
 
   public static void run_sim(MBTA mbta, Log log) {
-    throw new UnsupportedOperationException();
+    List<Thread> trainThreads = new ArrayList<>();
+    List<Thread> passThreads = new ArrayList<>();
+
+
+    for (Map.Entry<Train, List<Station>> mapEl : mbta.lines.entrySet()) {
+      Train t = mapEl.getKey();
+      trainThreads.add(new TrainThread(mbta, log, t));
+    }
+
+    for (Map.Entry<Passenger, List<Station>> mapEl : mbta.trips.entrySet()) {
+      Passenger p = mapEl.getKey();
+      passThreads.add(new PassengerThread(mbta, log, p));
+    }
+    
+    for (Thread t : trainThreads) {
+      t.start();
+    }
+
+    for (Thread p : passThreads) {
+      p.start();
+    }
+
+    for (Thread t : trainThreads) {
+      try {
+        t.join();
+      } catch(Exception e) {
+        throw new UnsupportedOperationException("join problems");
+      }
+    }
   }
 
   public static void main(String[] args) throws Exception {
