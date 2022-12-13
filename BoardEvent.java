301ac5fe001a24060cdc50b0ffdcea4884f9c56e
Quinnten P Silveira-Charbonnier <107774771+Quinnten@users.noreply.github.com>
2022-12-12 21:08:49 -0500

    Update BoardEvent.java

--- BoardEvent.java
+++ BoardEvent.java
@@ -30,36 +30,7 @@ public class BoardEvent implements Event {
     if (!mbta.lines.get(t).contains(mbta.nextStop.get(p)) || !mbta.stationPass.get(s).contains(p) || !mbta.trainStop.get(t).equals(s)) {
       throw new UnsupportedOperationException("Error in boarding");
     }
-
-
-
-  //   // Only move if the passenger is still traveling
-  //   System.out.println("Will this ven work?");
-
-  //   if (!s.getPassengers().contains(p)) {
-  //     throw new UnsupportedOperationException("The passenger " + p.toString() + " is not located at this station " +s.toString());
-  //   }
-    
-  //   if (p.currDest() == null) {
-  //     throw new UnsupportedOperationException("Passenger " + p.toString() + " is done with their journey and can no longer board");
-  //   } else if (!t.currStation().equals(s)) {
-  //     throw new UnsupportedOperationException("Can't board a passenger at a station they're not at");
-  //   }
-
-  //   // make sure that the train is going to the Passenger's destination
-  //   List<Station> stations = mbta.lines.get(t);
-  //   if (!stations.contains(p.currDest())) {
-  //     throw new UnsupportedOperationException(t.toString() + " will not bring " + p.toString() + "to their destination of " + p.currDest().toString());
-  //   }
-
-  //   /*update the state
-  //     Add passenger to train 
-  //     remove passenger from station 
-  //   */
-  //   t.addPassenger(p);
-  //   s.removePassenger(p);
-
-  // System.out.println("About to leave method?");
+    mbta.boardTrain(t, p, s);
    
   }
 }
